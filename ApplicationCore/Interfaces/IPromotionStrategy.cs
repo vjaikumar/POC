6b8b1046678b3e64a5bdf52017d23ad756cccf02
vjaikumar <vjaikumar@gmail.com>
2021-11-30 20:43:41 +0000

    Added new class for offer

---
+++ ApplicationCore/Interfaces/IPromotionStrategy.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IPromotionStrategy
+    {
+
+        bool CanExecute(ProductCheckout product, List<Promotion> promotions);
+
+      
+        double CalculateProductPrice(List<ProductCheckout> productCheckoutList);
+    }
+}
---
+++ ApplicationCore/PromotionStrategies/AdditionalItemOffer.cs
@@ -0,0 +1,47 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationCore.PromotionStrategies
+{
+    public class AdditionalItemOffer : IPromotionStrategy
+    {
+        private Promotion appliedPromotion;
+        private ProductCheckout ProductCheckout;
+
+        public AdditionalItemOffer()
+        {
+            appliedPromotion = new Promotion();
+            ProductCheckout = new ProductCheckout();
+        }
+
+        /// <summary>
+        /// Can Execute
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="promotions"></param>
+        /// <returns></returns>
+        public bool CanExecute(ProductCheckout product, List<Promotion> promotions)
+        {
+            
+
+            return false;
+        }
+
+        /// <summary>
+        /// Calculate ProductPrice
+        /// </summary>
+        /// <param name="productCheckoutList"></param>
+        /// <returns></returns>
+        public double CalculateProductPrice(List<ProductCheckout> productCheckoutList)
+        {
+            double finalPrice = 0;
+           
+
+            return finalPrice;
+        }
+    }
+}
