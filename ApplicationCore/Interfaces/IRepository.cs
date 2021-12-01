e7d1195986b9a3b7a984d00571e83788cf0ec735
vjaikumar <vjaikumar@gmail.com>
2021-12-01 20:42:35 +0000

    added IRepository

---
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities;
+
+using System.Collections.Generic;
+
+namespace ApplicationCore.Interfaces
+{
+   
+    public interface IRepository
+    {
+        
+        List<Product> GetAvilableProducts();
+
+       
+        List<Promotion> GetProductOffers();
+    }
+}
