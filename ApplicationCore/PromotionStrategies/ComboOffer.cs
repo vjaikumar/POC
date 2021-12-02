7b6cedb42aec498e5469014a88ee96ecfae6e3db
vjaikumar <vjaikumar@gmail.com>
2021-12-02 19:43:22 +0000

    addded combooffer

---
+++ ApplicationCore/PromotionStrategies/ComboOffer.cs
@@ -0,0 +1,95 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using PromotionEngine.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ApplicationCore.PromotionStrategies
+{
+    public class ComboOffer : IPromotionStrategy
+    {
+        Promotion appliedPromotion;
+        ProductCheckout recentProductCheckout;
+        List<ProductCheckout> productCheckouts;
+
+      
+        public bool CanExecute(ProductCheckout productCheckout, List<Promotion> promotions)
+        {
+            recentProductCheckout = productCheckout;
+            appliedPromotion = promotions.Where(x => x.ProductCode.Split(';').Contains(productCheckout.ProductCode)).FirstOrDefault();
+            if (appliedPromotion != null && !productCheckout.IsValidated && appliedPromotion.Type == Constants.Combo)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+      
+        public double CalculateProductPrice(List<ProductCheckout> productCheckoutList)
+        {
+            productCheckouts = new List<ProductCheckout>();
+
+            double finalPrice = 0;
+
+
+            try
+            {
+                string[] str = appliedPromotion.ProductCode.Split(';').ToArray();
+                foreach (ProductCheckout item in productCheckoutList)
+                {
+                    if (str.Contains(item.ProductCode))
+                    {
+                        productCheckouts.Add(item);
+                        item.IsValidated = true;
+                    }
+                }
+
+                int quantity_first = 0;
+                int quantity_second = 0;
+                if (productCheckouts.Count > 1)
+                {
+                    quantity_first = productCheckouts[0].Quantity;
+                    quantity_second = productCheckouts[1].Quantity;
+                }
+                
+                if (quantity_first == 0 || quantity_second == 0)
+                {
+                    return recentProductCheckout.DefaultPrice;
+
+                }
+
+               
+                if (quantity_first == quantity_second)
+                {
+                    finalPrice = appliedPromotion.Price * quantity_first;
+                }
+                else if (quantity_first > quantity_second)
+                {
+                    int additionalItems = quantity_first - quantity_second;
+                    finalPrice = (recentProductCheckout.DefaultPrice * additionalItems) + (appliedPromotion.Price * quantity_second);
+                }
+                else if (quantity_first < quantity_second)
+                {
+                    int additionalItems = quantity_second - quantity_first;
+                    finalPrice = (recentProductCheckout.DefaultPrice * additionalItems) + (appliedPromotion.Price * quantity_first);
+                }
+            }
+            catch (ArithmeticException ex)
+            {
+                
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+            return finalPrice;
+        }
+
+
+    }
+}
