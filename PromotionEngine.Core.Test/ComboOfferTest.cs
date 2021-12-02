72e54abb4b7970a5549b16225eb55ef55e143703
vjaikumar <vjaikumar@gmail.com>
2021-12-02 21:00:42 +0000

    testcase for combooffer

---
+++ PromotionEngine.Core.Test/ComboOfferTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApplicationCore.Entities;
+
+using ApplicationCore.Interfaces;
+using ApplicationCore.PromotionStrategies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.Core
+{
+    [TestClass]
+    public class ComboOfferTest
+    {
+
+        List<Promotion> _promotions;
+        IPromotionStrategy _promotionStrategy;
+        List<ProductCheckout> _productWithOffer;
+        List<ProductCheckout> _productWithoutOffer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _promotionStrategy = new ComboOffer();
+            _productWithoutOffer = new List<ProductCheckout>() { new ProductCheckout() { ProductCode = "C", Quantity = 1, DefaultPrice = 20 } };
+            _productWithOffer = new List<ProductCheckout>() { new ProductCheckout() { ProductCode = "C", Quantity = 1, DefaultPrice = 20 }, new ProductCheckout() { ProductCode = "D", Quantity = 1, DefaultPrice = 15 } };
+            _promotions = new List<Promotion>() { new Promotion() { Type = "Single", ProductCode = "A", Price = 130, Quantity = 3 }, new Promotion() { Type = "Single", ProductCode = "B", Price = 45, Quantity = 2 }, new Promotion() { Type = "Combo", ProductCode = "C;D", Price = 30, Quantity = 3 } };
+        }
+
+
+        [TestMethod]
+        public void Scenario_ComboOffer_WithOffer()
+        {
+            double expectedValue = 30;
+           
+
+        }
+
+        [TestMethod]
+        public void Scenario_ComboOffer_WithoutOffer()
+        {
+            double expectedValue = 20;
+            
+
+        }
+    }
+    
+}
