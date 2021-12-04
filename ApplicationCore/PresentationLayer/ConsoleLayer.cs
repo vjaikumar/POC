01ddd2fe72a06559d7181fe58636999a2f8a1e1e
vjaikumar <vjaikumar@gmail.com>
2021-12-04 20:57:22 +0000

    added console layer

---
+++ ApplicationCore/PresentationLayer/ConsoleLayer.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.PresentationLayer
+{
+    public class ConsoleLayer : IPromotionInputOutput
+    {
+        bool IPromotionInputOutput.DisplayTotalPrice(AppliedOffer appliedOffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<ProductCheckout> IPromotionInputOutput.LoadUserInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
