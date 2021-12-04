ec793bcde79ea1b5d0bdb8c0a4a61e31d68aecbf
vjaikumar <vjaikumar@gmail.com>
2021-12-04 20:54:51 +0000

    addesd new interface

---
+++ ApplicationCore/BusinessLayer/Facade.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.BusinessLayer
+{
+    public class Facade
+    {
+    }
+}
---
+++ ApplicationCore/Interfaces/IPromotionInputOutput.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationCore.Entities;
+using System.Collections.Generic;
+
+
+namespace ApplicationCore.Interfaces
+{
+   public interface  IPromotionInputOutput
+    {
+        List<ProductCheckout> LoadUserInput();
+
+        bool DisplayTotalPrice(Entities.AppliedOffer appliedOffer);
+    }
+}
