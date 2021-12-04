64e53cc21deace2b9f7cdc1607b41ccef53395fa
vjaikumar <vjaikumar@gmail.com>
2021-12-04 19:56:48 +0000

    testcase update

--- PromotionEngine.Core.Test/AdditionalItemOfferTest.cs
+++ PromotionEngine.Core.Test/AdditionalItemOfferTest.cs
@@ -8,6 +8,7 @@ using System.Text;
 
 namespace PromotionEngine.Core
 {
+    [TestClass]
     public class AdditionalItemOfferTest
     {
         List<Promotion> _promotions;
@@ -24,6 +25,50 @@ namespace PromotionEngine.Core
             _productWithOfferExtra = new ProductCheckout() { ProductCode = "A", Quantity = 4, DefaultPrice = 50 };
             _productWithoutOffer = new ProductCheckout() { ProductCode = "A", Quantity = 2, DefaultPrice = 50 };
             _promotions = new List<Promotion>() { new Promotion() { Type = "Single", ProductCode = "A", Price = 130, Quantity = 3 }, new Promotion() { Type = "Single", ProductCode = "B", Price = 45, Quantity = 2 }, new Promotion() { Type = "Combo", ProductCode = "C;D", Price = 30, Quantity = 3 } };
+        }
+
+        [TestMethod]
+        public void Scenario_AdditionalItemOffer_WithOffer()
+        {
+            List<ProductCheckout> orderCart = new List<ProductCheckout>();
+            orderCart.Add(_productWithOffer);
+            double expectedValue = 130;
+            bool canExecute = _promotionStrategy.CanExecute(_productWithOffer, _promotions);
+            if (canExecute)
+            {
+                double actualValue = _promotionStrategy.CalculateProductPrice(orderCart);
+                Assert.AreEqual(expectedValue, actualValue);
+            }
+
+        }
+
+        [TestMethod]
+        public void Scenario_AdditionalItemOffer_WithOffer_ExtraItems()
+        {
+            List<ProductCheckout> orderCart = new List<ProductCheckout>();
+            orderCart.Add(_productWithOfferExtra);
+            double expectedValue = 180;
+            bool canExecute = _promotionStrategy.CanExecute(_productWithOfferExtra, _promotions);
+            if (canExecute)
+            {
+                double actualValue = _promotionStrategy.CalculateProductPrice(orderCart);
+                Assert.AreEqual(expectedValue, actualValue);
+            }
+
+        }
+        [TestMethod]
+        public void Scenario_AdditionalItemOffer_WithoutOffer()
+        {
+            List<ProductCheckout> orderCart = new List<ProductCheckout>();
+            orderCart.Add(_productWithoutOffer);
+            double expectedValue = 100;
+            bool canExecute = _promotionStrategy.CanExecute(_productWithoutOffer, _promotions);
+            if (canExecute)
+            {
+                double actualValue = _promotionStrategy.CalculateProductPrice(orderCart);
+                Assert.AreEqual(expectedValue, actualValue);
+            }
+
         }
 
     }
