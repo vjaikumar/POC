d3f7a55d6d18fdb692e15003218308a4a11aafc8
vjaikumar <vjaikumar@gmail.com>
2021-12-04 19:38:37 +0000

    new test class AdditionalItemOfferTest

---
+++ PromotionEngine.Core.Test/AdditionalItemOfferTest.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using ApplicationCore.PromotionStrategies;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Core
+{
+    public class AdditionalItemOfferTest
+    {
+        List<Promotion> _promotions;
+        ProductCheckout _productWithOffer;
+        ProductCheckout _productWithoutOffer;
+        ProductCheckout _productWithOfferExtra;
+        IPromotionStrategy _promotionStrategy;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _promotionStrategy = new AdditionalItemOffer();
+            _productWithOffer = new ProductCheckout() { ProductCode = "A", Quantity = 3, DefaultPrice = 50 };
+            _productWithOfferExtra = new ProductCheckout() { ProductCode = "A", Quantity = 4, DefaultPrice = 50 };
+            _productWithoutOffer = new ProductCheckout() { ProductCode = "A", Quantity = 2, DefaultPrice = 50 };
+            _promotions = new List<Promotion>() { new Promotion() { Type = "Single", ProductCode = "A", Price = 130, Quantity = 3 }, new Promotion() { Type = "Single", ProductCode = "B", Price = 45, Quantity = 2 }, new Promotion() { Type = "Combo", ProductCode = "C;D", Price = 30, Quantity = 3 } };
+        }
+
+    }
+}
